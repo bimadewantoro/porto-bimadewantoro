67b4286d1a4fd28c87143e3748dfacd5f9cc1486
bimadewantoro <bimadewantoro22@gmail.com>
2023-03-21 22:05:18 +0700

    logout done

--- resources/js/components/admin/home/index.vue
+++ resources/js/components/admin/home/index.vue
@@ -1,6 +1,21 @@
+<script setup>
+    import { useRouter } from 'vue-router';
+
+    const router = useRouter();
+
+    const logout = () => {
+        localStorage.removeItem('token');
+        router.push({ path: '/' })
+    }
+</script>
+
 <template>
     <h1>Welcome to admin overview website</h1>
     <div>
         <router-link to="/">Home Pages</router-link>
     </div>
+    <br>
+    <button @click="logout">
+        Logout
+    </button>
 </template>
